6f366119e3f5763de1f55a7c06301484a59764f8
alschel <a.v.sheludkov@gmail.com>
2018-04-03 14:50:00 +0300

    n_users

--- TestData.R
+++ TestData.R
@@ -2,6 +2,9 @@
 # Author: Alexander Sheludkov
 # Date: 3 April 2018
 
+setwd("Desktop/JS/Around")
+
+
 library(sp)
 library(raster)
 library(rgdal)
@@ -10,7 +13,11 @@ library(dplyr)
 # reading data
 museums <- shapefile("data/museums.shp")
 plot(museums)
-museums@data %>% select(id, name) -> museums@data
+museums@data %>% select(id, name, opening_ho) -> museums@data
+
+museums@data %>% mutate(n_users = floor(runif(nrow(museums@data), min=1, max=101))) -> museums@data
+
+museums@data %>% View()
 
 # write
 writeOGR(museums, "data/moscow_museums.geojson", 
